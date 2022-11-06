59bba22caf9090eb76132ebb074d12ec98d59915
NotEternal <cent1pede@protonmail.ch>
2022-11-06 10:32:28 +0100

    refactor: remove excess title replacement

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -132,7 +132,9 @@ export const fetchDomainData = async (
     // @todo Temp solution
     const currentDomain = 'localhost' // getCurrentDomain()
     const { info, owner } = await storage.methods.getData(currentDomain).call()
+    console.log("🚀 chainId", chainId)
     const settings = parseSettings(info || '{}', chainId || 0)
+    console.log("🚀 settings", settings)
 
     fullData = { ...settings, admin: owner === ZERO_ADDRESS ? '' : owner }
 
